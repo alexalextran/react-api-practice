804d116437e17cd4b59442f78762147fd81fec36
alexalextran <alextran8385@gmail.com>
2022-01-18 23:23:08 +1100

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home'
 
 function App() {
   return (
+    <Router>
     <div className="App">
-    
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path=":id" element={<Posts />}></Route>
+    </Routes>
     </div>
+    </Router>
   );
 }
 
